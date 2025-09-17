32c24eb6cce0a674e0aba2ea073b55fdd7b6d2c9
Jill007798 <104267284+Jill007798@users.noreply.github.com>
2025-09-17 13:07:15 +0800

    Initial Commit

---
+++ Cooko/CookoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookoApp.swift
+//  Cooko
+//
+//  Created by Chang Jill on 2025/9/17.
+//
+
+import SwiftUI
+
+@main
+struct CookoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CookoTests/CookoTests.swift
@@ -0,0 +1,17 @@
+//
+//  CookoTests.swift
+//  CookoTests
+//
+//  Created by Chang Jill on 2025/9/17.
+//
+
+import Testing
+@testable import Cooko
+
+struct CookoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
